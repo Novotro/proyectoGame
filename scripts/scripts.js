6c8dadfe3880e1c1c08a73fe92528f55a6b5f905
Novotro <rubendr91@gmail.com>
2019-06-19 08:10:56 +0000

    Cambios en estilos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -51,30 +51,46 @@ function resize() {
  }
 }
 
-var group;
-var dwarf;
+
+
+//Enanos
+var cat;
+var catStrong;
+//Eventos
 var timedEvent;
 var timedEventSucesos;
+//Contadores
 var contadorNormal = 0;
+var contadorStrong = 0;
 var contadorMaximo = 0;
+//Limitadores
 var maximoNormal = 10;
-var maximoFuerte = 10;
+var maximoStrong = 10;
+//Variables para recursos
 var recursos = 100;
 var delay = 1000;
 var sucesos = 10000;
 var mejoras = 1;
-var costesMejora = 50;
 var temporizador = 0;
+var total= 0;
+var recursosSegundo;
+// Costes
+var costesMejora = 50;
+var costeCat = 50;
+var costeCatStrong = 100;
+// Otros
+var group;
+
 
 function preload(){
   //Fondo del juego
   this.load.image("background", "./assets/graphics/background.jpg");
-  this.load.image("dwarf", "./assets/graphics/dwarf.png");
+  this.load.image("cat", "./assets/graphics/cat.png");
 }
 
 function create(){
   // Background del juego
-  background = this.add.image(0, 0, "background").setOrigin(0, 0).setScale(1.5);
+  background = this.add.image(0, 0, "background").setOrigin(0, 0).setScale(2);
   // Limites del mapa
   this.physics.world.setBounds(0, 0, 660, 340);
 
@@ -93,8 +109,11 @@ function create(){
     collideWorldBounds: true
   });
 
-  dwarf = this.add.group();
-  dwarf = this.physics.add.group();
+  cat = this.add.group();
+  cat = this.physics.add.group();
+
+  catStrong = this.add.group();
+  catStrong = this.physics.add.group();
 
   // Eventos secuenciales
   //Evento que va contando los recursos
@@ -106,7 +125,9 @@ function create(){
   });
 
   //Eventos de botones
-  $(".normal").click({ game: this, tipo: "normal" }, createDwarf);
+  $(".normal").click({ game: this, tipo: "normal" }, createcat);
+  $(".fuerte").click({ game: this, tipo: "fuerte" }, createcat);
+
 
   $(".mejorar").click(function(e) {
     e.preventDefault();
@@ -119,36 +140,81 @@ function update(){
 }
 
 // Funciones para el juego
-function createDwarf(event){
+function createcat(event){
   switch (event.data.tipo) {
     case "normal":
-        if (recursos - 50 >= 0 && contadorNormal < maximoNormal) {
-            dwarf[contadorNormal] = group.create(100, 100, 'dwarf').setVelocity(Math.random() * 100 - 100, Math.random() * 100 - 100).setScale(0.32);
-            recursos = recursos - 50;
-            actualizarRecursos();
+        if (recursos - costeCat >= 0 && contadorNormal < maximoNormal) {
+            cat[contadorNormal] = group.create(100, 100, 'cat').setVelocity(Math.random() * 100 - 100, Math.random() * 100 - 100).setScale(0.32);
+            recursos = recursos - costeCat;
             contadorNormal++;
+            costeCat =  Math.trunc(costeCat *1.7);
+            $(".total").html(contadorNormal);
+            $(".costeEnanos").html(costeCat);
+            actualizarRecursos();
         }
+    break;
+     case "fuerte":
+        if (recursos - costeCatStrong >= 0 && contadorStrong < maximoStrong) {
+            catStrong[contadorStrong] = group.create(100, 100, 'cat').setVelocity(Math.random() * 25 - 50, Math.random() * 25 - 50).setScale(0.32);
+            recursos = recursos - costeCatStrong;
+            contadorStrong++;
+            costeCatStrong =  Math.trunc(costeCatStrong *1.7);
+            $(".totalFuerte").html(contadorStrong);
+            $(".costeEnanosFuerte").html(costeCatStrong);
+            actualizarRecursos();
+       }
+    break;
+    case "rapido":
+            // En construccion
+       if (recursos - costeCatStrong >= 0 && contadorStrong < maximoStrong) {
+            catStrong[contadorStrong] = group.create(100, 100, 'cat').setVelocity(Math.random() * 25 - 50, Math.random() * 25 - 50).setScale(0.32);
+            recursos = recursos - costeCatStrong;
+            contadorStrong++;
+            costeCatStrong =  Math.trunc(costeCatStrong *1.7);
+            $(".totalFuerte").html(contadorStrong);
+            $(".costeEnanosFuerte").html(costeCatStrong);
+            actualizarRecursos();
+       }
     break;
   }
 }
 
-// Funcion que va actualizando los recursos
-function actualizarRecursos() {
-  recursos = recursos + contadorNormal + contadorMaximo;
-  $(".recursos").html(recursos);
-  $(".recursosSegundo").html((contadorNormal + contadorMaximo * 2) * mejoras);
+
+
+
+
+//Funcion para la IA de los Enanos
+function iacat(){
+
 }
 
+// Funcion que va actualizando los recursos
+function actualizarRecursos() {
+  recursos = Math.trunc(recursos + (contadorNormal * 2) + (contadorStrong * 4));
+  recursosSegundo = Math.trunc(((contadorNormal * 2) + (contadorStrong * 4)) / (delay/1000) *2);
+  $(".recursos").html(recursos);
+  $(".recursosSegundo").html(recursosSegundo);
+}
 
 function mejoraRecursos(timedEvent) {
   if (recursos > costesMejora && mejoras < 10 && contadorNormal > 0) {
     mejoras++;
     recursos = recursos - costesMejora;
     costesMejora = Math.trunc(costesMejora * 1.5);
-    $(".nivelMejora").html(mejoras);
-    $(".recursos").html(recursos);
-    $(".proximaMejora").html(costesMejora);
+    costeCat = costeCat * 1.25;
+    if(mejoras >= 10){
+     $(".nivelMejora").html("MAX");
+     $(".recursos").html("MAX");
+     $(".proximaMejora").html("MAX");
+     $(".costeEnanos").html("MAX");
+    }else{
+     $(".nivelMejora").html(mejoras);
+     $(".recursos").html(recursos);
+     $(".proximaMejora").html(costesMejora);
+    }
     timedEvent.delay = timedEvent.delay - timedEvent.delay * 0.25;
+
+    delay = timedEvent.delay;
   }
 }
 
